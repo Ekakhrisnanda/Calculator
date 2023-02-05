754cea78717cdd73a2c07c0b8c7d3f6d0be55792
Eduk <ekakhrisnanda@gmail.com>
2023-02-05 15:55:14 +0700

    Create the operate function

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const operate = function(a, b, operator) {
+    switch(operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return substract(a, b);
+            break;
+        case 'x':
+            return multiply(a, b);
+            break;
+        case ':':
+            return divide(a, b);
+    }
+}
